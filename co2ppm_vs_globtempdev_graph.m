5c96e3a137f8158385ffe1b7785f95d81ebcccd9
wbed <83097253+wbed@users.noreply.github.com>
2024-11-16 12:17:36 +0000

    Update co2ppm_vs_globtempdev_graph.m
    adding sources to graph

--- co2ppm_vs_globtempdev_graph.m
+++ co2ppm_vs_globtempdev_graph.m
@@ -10,10 +10,10 @@ title('Correlation between concentration of CO_2 and Global Air Temperature Anom
 yyaxis left
 plot(year, co2ppm, 'b', 'DisplayName', 'CO_2(ppm)')
 xlabel('Year')
-ylabel('Concentration of Carbon Dioxide at Sea Level (ppm)')
+ylabel('Concentration of CO_2 at Sea Level (ppm) [1, 2, 3]')
 yyaxis right
 plot(year, avgTemp, 'r', 'DisplayName', 'Temperature')
-ylabel('Global Air Temperature Anomaly')
+ylabel('Global Air Temperature Anomaly [4]')
 hold off
 
 legend('Location', 'northwest')
